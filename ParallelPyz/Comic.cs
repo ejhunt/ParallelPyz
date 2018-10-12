02ffcdc26e05851458c2ac6c6bd29e66813258d5
Edward J. Hunt <ejhunt@ejhunt.net>
2018-10-12 10:50:46 -0400

    zz and xml removed

---
+++ ParallelPyz/Comic.cs
@@ -0,0 +1,83 @@
+﻿using ImageProcessor;
+using ImageProcessor.Imaging.Formats;
+using ImageProcessor.Plugins.WebP.Imaging.Formats;
+using SharpCompress.Archives;
+using SharpCompress.Archives.Zip;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+namespace ParallelPyz {
+
+    class Comic {
+        string filepath;
+        string dir_root;
+        public string file_base;
+        public string temp_dir;
+        string out_path = @"d:\webpcomics";
+        public MemoryStream converted = new MemoryStream();
+
+        public Comic(string _filepath) {
+            filepath = _filepath;
+            dir_root = Path.GetDirectoryName(filepath);
+        }
+
+        public void ExtractArchive() {
+            var archive = ArchiveFactory.Open(filepath);
+            temp_dir = GetTemporaryDirectory();
+            foreach (var entry in archive.Entries) {
+                if (!entry.IsDirectory) {
+                    string ek = entry.Key.ToLower();
+
+                    if (entry.Key.ToLower().StartsWith("zz")) {
+                        continue;
+                    }
+                    
+                    file_base = Path.GetFileNameWithoutExtension(entry.Key);
+                    MemoryStream memStream = new MemoryStream();
+                    entry.WriteTo(memStream);
+                    //Page temp_page = new Page(memStream, entry.Key);
+                    //orig_pages.Add(temp_page);       
+                    ISupportedImageFormat format = new WebPFormat { Quality = 80 };
+                    ImageFactory image = new ImageProcessor.ImageFactory();
+                    try
+                    {
+                        image.Load(memStream.ToArray())
+                                    .Format(format)
+                                    .Save(converted);
+                    }
+                    catch {
+                        continue;
+                    }
+                    string filename = Path.Combine(temp_dir, file_base + ".webp");
+                    using (System.IO.FileStream output = new System.IO.FileStream(filename, FileMode.Create)) {
+                        converted.CopyTo(output);
+                        memStream.Dispose();
+                        output.Close();
+                    }
+                }
+            }
+            CompressArchive();
+        }
+
+        public void CompressArchive() {
+            string outFile = Path.GetFileNameWithoutExtension(filepath) + ".cbz";
+            using (var archive = ZipArchive.Create()) {
+                archive.AddAllFromDirectory(temp_dir);
+                using (Stream newStream = File.Create(Path.Combine(out_path, outFile))) {
+                    archive.SaveTo(newStream, SharpCompress.Common.CompressionType.LZMA);
+                    newStream.Dispose();
+                    System.Console.WriteLine(Path.Combine(out_path, outFile));
+                }
+            }         
+            Directory.Delete(temp_dir, true);
+        }
+
+        static string GetTemporaryDirectory() {
+            string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(tempDirectory);
+            return tempDirectory;
+        }
+
+    }
+}
--- ParallelPyz/Program.cs
+++ ParallelPyz/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ParallelPyz
-{
+namespace ParallelPyz {
     class Program
     {
+        static public string path;
         static void Main(string[] args)
         {
+            try
+            {
+                path = Directory.GetCurrentDirectory();
+                Console.WriteLine(path);
+                string[] filelist = Directory.GetFiles(path);
+                Comic comic;
+                //foreach (string file in filelist)
+                Parallel.ForEach(filelist, (file) =>
+                {
+                    if (Path.GetExtension(file).Equals(".cbr") || Path.GetExtension(file).Equals(".cbz"))
+                    {
+                        comic = new Comic(file);
+                        comic.ExtractArchive();
+                    }
+                });
+                Console.WriteLine("Complete!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
         }
     }
 }
